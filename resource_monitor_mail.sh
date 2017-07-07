b68c67a4c91a1a3b7fc4e606373226f78e8156dd
UEHARA <juehara@gs6151-isfsadev-198120228166.ndc.nasa.gov>
2017-07-07 10:40:35 -0400

    adding mailer

---
+++ resource_monitor_mail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to see if the monitoring was tripped by creating sentinel temp file
+tmpfile="resource_monitor.tmp"
+if [ -e $tmpfile ]; then
+  ./resource_monitor > $tmpfile
+else
+  touch $tmpfile
+  chmod 666 $tmpfile
+fi
