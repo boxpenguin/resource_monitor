08e23e25dc42d1657344f39d7bd18c1c26664929
UEHARA <juehara@gs6151-isfsadev-198120228166.ndc.nasa.gov>
2017-07-07 11:03:04 -0400

    Final code to mailer

--- resource_monitor_mail.sh
+++ resource_monitor_mail.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 # Script to see if the monitoring was tripped by creating sentinel temp file
+
 tmpfile="resource_monitor.tmp"
+host=$(hostname)
+mailarray="Jordan.C.Uehara@nasa.gov"
+LOG="/var/log/resource_monitor.log"
 if [ -e $tmpfile ]; then
+  rm -f $tmpfile 2>/dev/null
+  touch $tmpfile
+  chmod 666 $tmpfile
   ./resource_monitor $tmpfile
 else
   touch $tmpfile
   chmod 666 $tmpfile
+  ./resource_monitor $tmpfile
 fi
+
+# Send output to varlog regardless!
+cat $tmpfile >> $LOG
+lines=$(wc -l $tmpfile| awk '{print $1}')
+# If there is more than the default one line then mail an alert
+if [ $lines -ne 1 ]; then
+  cat $tmpfile | mail -s "Alert on Host: $host" $mailarray
+fi
+
+rm -f $tmpfile
