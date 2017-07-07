b8b62d8c5cb6758332ba788e5d31151ecea179ea
UEHARA <juehara@gs6151-isfsadev-198120228166.ndc.nasa.gov>
2017-07-07 10:43:49 -0400

    i am an idiot

--- resource_monitor_mail.sh
+++ resource_monitor_mail.sh
@@ -3,7 +3,7 @@
 # Script to see if the monitoring was tripped by creating sentinel temp file
 tmpfile="resource_monitor.tmp"
 if [ -e $tmpfile ]; then
-  ./resource_monitor > $tmpfile
+  ./resource_monitor $tmpfile
 else
   touch $tmpfile
   chmod 666 $tmpfile
